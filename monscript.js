f53b357eeb401ce38c0863d2267c1250cf18da99
Ryûga <corentingambier@gmail.com>
2025-01-13 11:49:18 +0100

    TP7 fini

--- monscript.js
+++ monscript.js
@@ -5,3 +5,5 @@ nomSerie = prompt("Quelle est ta série préférée ? ");
 nomPerso = prompt("Quelle est ton personnage favori ? ");
 
 alert("Ma série préférée est " + nomSerie + " et mon personnage favori est " + nomPerso + " !");
+
+// TP7 fini
