0a7ba61a3d85899867d146cdd7d40cbcca789f5c
Ryûga <corentingambier@gmail.com>
2025-01-13 10:34:36 +0100

    TP3 fini

---
+++ monscript.js
@@ -0,0 +1,7 @@
+alert('Ceci viens de mon fichier Js');
+
+let reponse = prompt('Quelle est ta série préférée ?');
+console.log(reponse);
+
+//6)
+// résultat je peux voir la réponse de l'utilisateur enregistrée en session temporaire dans la console via inspecter
