1c7a0812f7c7c3e7d61bcbee3caa424db622d738
Ryûga <corentingambier@gmail.com>
2025-01-13 11:46:22 +0100

    TP6 fini

--- monscript.js
+++ monscript.js
@@ -1,28 +1,17 @@
-let nomJour = "Lundi",
-    numJour = 13,
-    nomMois = "Janvier",
-    isHiver = true;
-//ça fonctionne
-console.log(`Nous sommes le ${nomJour} ${numJour} ${nomMois} 2025 et c'est la saison ${isHiver ? 'hivernale' : 'éstivale'}`)
+let nbr1,nbr2,nbr3,nbr4;
 
+nbr1 = 3;
+nbr2 = 7;
+nbr3 = 8;
+nbr4 = 21;
 
-let tabWeek = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+console.log(nbr1 + nbr2 );
+console.log(nbr4 - nbr3 );
 
-let tabMonths = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+nbr2 += nbr1;
 
-let myDate1 = {
-    dayname: tabWeek[0],
-    daynum: 13,
-    monthname: tabMonths[0],
-    isWinter: true
-}
+console.log(nbr2++);
 
-console.log(`Nous sommes le ${myDate1.dayname} ${myDate1.daynum} ${myDate1.monthname} 2025 et c'est la saison ${myDate1.isWinter ? 'hivernale' : 'éstivale'}`)
+console.log(nbr4/nbr1);
 
-let MyDateV2 = {};
-MyDateV2.dayname2 = tabWeek[0];
-MyDateV2.daynum2 = 13;
-MyDateV2.monthname2 = tabMonths[0];
-MyDateV2.isWinter2 = true;
-
-console.log(`Nous sommes le ${MyDateV2.dayname2} ${MyDateV2.daynum2} ${MyDateV2.monthname2} 2025 et c'est la saison ${MyDateV2.isWinter2 ? 'hivernale' : 'éstivale'}`)
+console.log(nbr4 % nbr1);
