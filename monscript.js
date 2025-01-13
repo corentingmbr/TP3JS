bad312572dbd4c7f8633cdcade346d66e5b42926
Ryûga <corentingambier@gmail.com>
2025-01-13 11:49:04 +0100

    TP6 fini

--- monscript.js
+++ monscript.js
@@ -1,17 +1,7 @@
-let nbr1,nbr2,nbr3,nbr4;
+let nomSerie;
+let nomPerso;
 
-nbr1 = 3;
-nbr2 = 7;
-nbr3 = 8;
-nbr4 = 21;
+nomSerie = prompt("Quelle est ta série préférée ? ");
+nomPerso = prompt("Quelle est ton personnage favori ? ");
 
-console.log(nbr1 + nbr2 );
-console.log(nbr4 - nbr3 );
-
-nbr2 += nbr1;
-
-console.log(nbr2++);
-
-console.log(nbr4/nbr1);
-
-console.log(nbr4 % nbr1);
+alert("Ma série préférée est " + nomSerie + " et mon personnage favori est " + nomPerso + " !");
