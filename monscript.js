e3a52072056ee07640fd0cd198146ca0104ad1e6
Ryûga <corentingambier@gmail.com>
2025-01-21 14:48:58 +0100

    TP10 fini

--- monscript.js
+++ monscript.js
@@ -2,14 +2,39 @@ let age;
 
 age = prompt('What is your age?');
 
-if (age < 18) {
-    console.log(`The user has ${age} years old, he is minor`);
+if (age === 18) {
+    console.log(`The user has ${age} years old, he just got his majority !`);
+} else if (age === 25) {
+    console.log(`The user has ${age} years old, he has a quarter of a century !`);
+} else if (age === 50) {
+    console.log(`The user has ${age} years old, he is half a century !`);
+} else if (age === 62) {
+    console.log(`The user has ${age} years old, he just got retired !`);
+} else if (age === 100) {
+    console.log(`The user has ${age} years old, he just became a centenarian !`);
 }
-if (age >= 18) {
-    console.log(`The user has ${age} years old, he is major`);
-}
-if(age>=62){
-    console.log(`The user has ${age} years old, he is a major but also retired !`);
+
+// méthode switch.
+
+switch (age) {
+    case 18:
+        console.log(`The user has ${age} years old, he just got his majority !`);
+        break;
+    case 25:
+        console.log(`The user has ${age} years old, he has a quarter of a century !`);
+        break;
+    case 50:
+        console.log(`The user has ${age} years old, he is half a century !`);
+        break;
+    case 62:
+        console.log(`The user has ${age} years old, he just got retired !`);
+        break;
+    case 100:
+        console.log(`The user has ${age} years old, he just became a centenarian !`);
+        break;
+    default:
+        console.log(`The user has ${age} years old`);
+        break;
 }
 
 
