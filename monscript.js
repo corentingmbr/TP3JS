ea013ce93d2b98e3d784b9f4ce6fc78b252921e6
Ryûga <corentingambier@gmail.com>
2025-01-21 14:42:24 +0100

    TP9 fini

--- monscript.js
+++ monscript.js
@@ -1,11 +1,18 @@
-let number = 5;
-let text = '5';
-let isRainingToday = true;
+let age;
 
-console.log(number==text); // true
-console.log(number===text); // false
+age = prompt('What is your age?');
 
-if(isRainingToday){
-    console.log('Where is my umbrella?');
+if (age < 18) {
+    console.log(`The user has ${age} years old, he is minor`);
 }
+if (age >= 18) {
+    console.log(`The user has ${age} years old, he is major`);
+}
+if(age>=62){
+    console.log(`The user has ${age} years old, he is a major but also retired !`);
+}
+
+
+
+
 
